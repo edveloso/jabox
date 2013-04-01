a04f75bf234c75c739afabad4cc9b80e1f773647
spiddy <d.kapanidis@gmail.com>
2013-04-01 19:47:19 +0200

    fixed Git Executor to correctly add files to index

--- scm-git/src/main/java/org/jabox/scm/git/GITFacade.java
+++ scm-git/src/main/java/org/jabox/scm/git/GITFacade.java
@@ -21,146 +21,36 @@ package org.jabox.scm.git;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 import org.jabox.model.Project;
 
 public class GITFacade {
-	// private final SVNClientManager _clientManager;
 
-	public GITFacade() {
-	}
+    public GITFacade() {
+    }
 
-	public boolean validate(String url, String username, String password)
-			throws IOException {
-		// GITConnectorConfig svnc = new GITConnectorConfig();
-		// svnc.username = username;
-		// svnc.password = password;
-		// svnc.server = new Server();
-		// svnc.server.setUrl(url);
-		// try {
-		// // Authenticate
-		// ISVNAuthenticationManager authManager = new
-		// BasicAuthenticationManager(
-		// svnc.getUsername(), svnc.getPassword());
-		// SVNRepository repo = SVNRepositoryFactory.create(svnc.getSvnDir());
-		// repo.setAuthenticationManager(authManager);
-		// repo.testConnection();
-		// return true;
-		// } catch (SVNException e) {
-		// return false;
-		// }
-		return true;
-	}
+    public boolean validate(final String url, final String username,
+            final String password) throws IOException {
+        return true;
+    }
 
-	/**
-	 * Checks out the base-dir of the git
-	 * 
-	 * @param storePath
-	 *            the path where to store the git base-dir.
-	 */
-	public void checkoutBaseDir(final File storePath,
-			final IGITConnectorConfig gitc) {
+    /**
+     * Checks out the base-dir of the git
+     * 
+     * @param storePath
+     *            the path where to store the git base-dir.
+     */
+    public void checkoutBaseDir(final File storePath,
+            final IGITConnectorConfig gitc) {
 
-		GITRepository.initialize();
-		// _clientManager.createRepository(svnc.getSvnDir(), true);
-		//
-		// // Authenticate
-		// ISVNAuthenticationManager authManager = new
-		// BasicAuthenticationManager(
-		// svnc.getUsername(), svnc.getPassword());
-		// _clientManager.setAuthenticationManager(authManager);
-		//
-		// _clientManager.getUpdateClient().doCheckout(svnc.getSvnDir(),
-		// storePath, SVNRevision.UNDEFINED, SVNRevision.HEAD,
-		// SVNDepth.INFINITY, false);
-	}
+        GITRepository.initialize();
+    }
 
-	public void commitProject(final Project project, final File tmpDir,
-			final IGITConnectorConfig svnc) {
-		File projectDir = new File(tmpDir, project.getName());
-		Executor.exec("git init", null, projectDir);
-		try {
-			Executor.exec("git  add .", null, projectDir, true, true, 1000);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// XXX This is blocked for some reason. Swallow the Timeout for the
-			// moment, needs further investigation.
-		}
-		try {
-			Executor.exec("git commit . -m \"initial commit\"", null,
-					projectDir, true, true, 1000);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// XXX This is blocked for some reason. Swallow the Timeout for the
-			// moment, needs further investigation.
-		}
-
-		// // Add files (svn add)
-		// SVNWCClient wcClient = _clientManager.getWCClient();
-		// wcClient.doAdd(new File(tmpDir, project.getName()), false, false,
-		// false, SVNDepth.INFINITY, false, true);
-		//
-		// // Commit files (svn commit)
-		// SVNCommitClient commitClient = _clientManager.getCommitClient();
-		// ISVNAuthenticationManager authManager = new
-		// BasicAuthenticationManager(
-		// svnc.getUsername(), svnc.getPassword());
-		// _clientManager.setAuthenticationManager(authManager);
-		// File[] paths = new File[1];
-		// paths[0] = new File(tmpDir, project.getName());
-		// // paths[1] = new File("tags");
-		// // paths[2] = new File("branches");
-		//
-		// setProjectProperties(project, tmpDir);
-		//
-		// commitClient.doCommit(paths, false, "[JABOX] Initial Commit", null,
-		// null, false, true, SVNDepth.INFINITY);
-	}
-
-	/**
-	 * Set the bugtraq and svn:ignore properties to the project.
-	 * 
-	 * @param project
-	 * @param rootDir
-	 * @throws SVNException
-	 */
-	private void setProjectProperties(final Project project, final File rootDir) {
-		// File moduleFile = new File(rootDir, File.separator +
-		// project.getName()
-		// + File.separator + "trunk" + File.separator + project.getName());
-		// setModuleProperties(moduleFile);
-	}
-
-	/**
-	 * Set the bugtraq and snv:ignore properties to the module directory.
-	 * 
-	 * @param dir
-	 * @throws SVNException
-	 */
-	private void setModuleProperties(final File dir) {
-		// SVNWCClient wc = _clientManager.getWCClient();
-		// // setSVNProperty(wc, dir, "bugtraq:number", "true");
-		// // setSVNProperty(wc, dir, "bugtraq:append", "false");
-		// // setSVNProperty(wc, dir, "bugtraq:message", "refs %BUGID%");
-		// // setSVNProperty(wc, dir, "bugtraq:label", "Issue:");
-		// // setSVNProperty(wc, dir, "bugtraq:url",
-		// // "http://localhost/redmine/issues/show/%BUGID%");
-		// // setSVNProperty(wc, dir, "bugtraq:warnifnoissue", "true");
-		// setSVNProperty(wc, dir, "svn:ignore", "target");
-	}
-
-	// private void setSVNProperty(final SVNWCClient wc, final File dir,
-	// final String key, final String value) throws SVNException {
-	// wc.doSetProperty(dir, key, SVNPropertyValue.create(value), false,
-	// SVNDepth.EMPTY, ISVNPropertyHandler.NULL, null);
-	// }
+    public void commitProject(final Project project, final File tmpDir,
+            final IGITConnectorConfig svnc) {
+        File projectDir = new File(tmpDir, project.getName());
+        Executor.exec("git init", null, projectDir);
+        Executor.exec("git  add -A", null, projectDir);
+        Executor.exec("git commit -m Initial", null, projectDir);
+    }
 }
