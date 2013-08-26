ec1c400f0fd948075d9de48eac147468aa25cf91
spiddy <d.kapanidis@gmail.com>
2013-08-26 09:02:25 +0300

    fixes #43 The SettingsModifier now replace keys with empty string when null values exist

--- jabox-persistence/src/main/java/org/jabox/utils/SettingsModifier.java
+++ jabox-persistence/src/main/java/org/jabox/utils/SettingsModifier.java
@@ -5,10 +5,14 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.Map;
 import java.util.Map.Entry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.apache.wicket.util.io.IOUtils;
 
 public class SettingsModifier {
+    private static final Logger LOGGER = LoggerFactory
+        .getLogger(SettingsModifier.class);
 
     public static String parseInputStream(final InputStream is,
             Map<String, String> values) throws IOException {
@@ -17,7 +21,11 @@ public class SettingsModifier {
         String theString = writer.toString();
         String replace = theString;
         for (Entry<String, String> pair : values.entrySet()) {
+            if (pair.getValue()==null) {
+                pair.setValue("");
+            }
             replace = replace.replace(pair.getKey(), pair.getValue());
+            LOGGER.debug("Settings.xml variable: {}={}", pair.getKey(), pair.getValue());
         }
         return replace;
     }
